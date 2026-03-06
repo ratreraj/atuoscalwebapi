00360a1824e297b35546ab3055495adfd4429cad
rajratre <rajprakashratre@gmail.com>
2026-03-07 01:01:09 +0530

    Add project files.

---
+++ atuoscalwebapi/Controllers/OrdersController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+
+namespace atuoscalwebapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public OrdersController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder(Order order)
+        {
+            var connectionString = _config.GetConnectionString("Default");
+
+            using SqlConnection conn = new SqlConnection(connectionString);
+
+            await conn.OpenAsync();
+
+            var cmd = new SqlCommand(
+                "INSERT INTO Orders(CustomerName,ProductName,CreatedDate) VALUES(@c,@p,GETDATE())",
+                conn);
+
+            cmd.Parameters.AddWithValue("@c", order.CustomerName);
+            cmd.Parameters.AddWithValue("@p", order.ProductName);
+
+            await cmd.ExecuteNonQueryAsync();
+
+            return Ok("Order Created");
+        }
+    }
+}
---
+++ atuoscalwebapi/Models/Order.cs
@@ -0,0 +1,8 @@
+﻿namespace atuoscalwebapi.Models
+{
+    public class Order
+    {
+        public string CustomerName { get; set; }
+        public string ProductName { get; set; }
+    }
+}
